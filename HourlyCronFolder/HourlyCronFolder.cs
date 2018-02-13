f648fff8f497fbae16a91e8ce0ed93f2f2c26a2c
Scott Brettle <srbrettle@gmail.com>
2018-02-13 20:39:23 +0000

    Add project files.

---
+++ HourlyCronFolder/HourlyCronFolder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Toolkit;
+
+namespace HourlyCronFolder
+{
+    public partial class HourlyCronFolder : ServiceBase
+    {        
+        public HourlyCronFolder()
+        {                        
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {            
+            RunCronFolder();
+            // Set up timer
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000*60;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            RunCronFolder();
+        }
+
+        public void RunCronFolder()
+        {            
+            string folderPath;
+            string[] files;
+            try
+            {
+                folderPath = ("C:\\Cron");                
+                files = Directory.GetFiles(folderPath);
+                Directory.SetCurrentDirectory(folderPath);               
+
+                foreach (string file in files)
+                {
+                    // the name of the application to launch
+                    String applicationName = file;
+
+                    // launch the application
+                    ApplicationLoader.PROCESS_INFORMATION procInfo;
+                    ApplicationLoader.StartProcessAndBypassUAC(applicationName, out procInfo);                    
+                }
+            }
+            catch { }            
+        }
+    }
+}
---
+++ HourlyCronFolder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HourlyCronFolder
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>        
+        static void Main()
+        {            
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HourlyCronFolder()
+            };
+            ServiceBase.Run(ServicesToRun);            
+        }                        
+    }
+}
